cea88d852f4c5681cef09099b1c0a2793853e6da
John DRO <baptiste.duprat@u-psud.fr>
2016-03-11 20:00:46 +0100

    open-blog

---
+++ blog/js/globals.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var USERNAME = 'JohnDRO', // github username
+    REPO = 'issuance', // github repository
+    TOKEN = '639c31a2f258cedcf87b60535b935eb708dad8de '; // github app access token
